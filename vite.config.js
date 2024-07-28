bf5bd6fb9bccb02f405e988f2ac9aa8b1b6729e1
Pritam Chakraborty <143912541+itssodope01@users.noreply.github.com>
2024-07-29 00:31:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-app-react/', // Replace with your repo name
+  base: '/weather-app-react/', 
 });
