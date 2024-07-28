b8d46b17455e9218a1596682415db3cb1f224086
Pritam Chakraborty <pritam.amit29@gmail.com>
2024-07-29 00:38:13 +0200

    Initial commit.v7

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header.jsx';
-import Main from './components/Main.jsx';
+import Weather from './components/Weather.jsx';
 import './styles/components/App.scss';
 import { useContext } from 'react';
 import ThemeContext from './context/theme.context';
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className={`App-${darkMode ? 'dark' : 'light'}`}>
       <Header />
-      <Main />
+      <Weather />
     </div>
   );
 }
